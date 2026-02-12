e42222489da1dc1dbd2e6d86f80ea94facf350ec
Camilo Alarcon <camilo4750@gmail.com>
2026-02-12 16:41:47 -0500

    Update PHPUnit configuration to include tests for the Clients module, and register ClientManagerProvider in the service provider. Enhance OpenAPI documentation with authentication details and new endpoints for client management.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     Internal\Auth\Infrastructure\Http\Providers\AuthManagerProvider::class,
     Internal\Users\Infrastructure\Http\Providers\UserManagerProvider::class,
+    Internal\Clients\Infrastructure\Http\Providers\ClientManagerProvider::class,
 ];
